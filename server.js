2a359ffe1b41d7153f1fc3790e1e16c45da073c4
“Matt <matthewsugden@msn.com>
2024-03-03 10:35:52 +0000

    Database connection to MongoDB

--- server.js
+++ server.js
@@ -1,36 +1,29 @@
-if (process.env.NODE_ENV !== 'production') { 
-    require('dotenv').config() 
-}
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const connectDB = require('./config/dbConnection')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
-const mongoose = ()=>{
-    mongoose.connect(process.env.DATABASE_URL) // Replaced mongoose.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`)
-    .then(()=>{
-       console.log("DB connection successful.");
-    })
-    .catch((err)=>{
-       console.log(`DB connection error:${err}`);
-    });
- }
-
-// const mongoose = require('mongoose')
-// mongoose.connect(process.env.DATABASE_URL, {
-//     useNewUrlParser: true })
-//     const db = mongoose.connection
-//     db.on('error', error => console.error(error))
-//     db.once('open', () => console.log('Connected to Mongoose'))
+// DB Connection
+connectDB()
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
-app.listen(process.env.PORT || 3000)
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(process.env.PORT || 3000, console.log('Server running on port 3000'))
+})
